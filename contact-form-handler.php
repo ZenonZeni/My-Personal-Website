4928534ae531d50d3a080d1e8f97c3498bd66560
Christopher Nguyen <ZenonZeni@hotmail.com>
2021-02-15 17:59:02 -0500

    changes

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,19 +1,27 @@
 <?php
+
       $name = $_POST['name'];
       $visitor_email = $_POST['email'];
       $country = $_POST['country'];
       $subject = $_POST['subject'];
 
       $email_from = 'zenonzeni.ca';
-      $email_subject = "New Form Submission"
+
+      $email_subject = "New Form Submission";
+
       $email_body = "User Name: $name. \n".
                     "User Email: $visitor_email.\n"
                     "User country: $country. \n"
                     "User subject: $subject. \n";
 
     $to = "zenonzeni@gmail.com";
+
     $headers = "From: $email_from \r\n";
+
     $headers .= "Reply-To: $visitor_email \r\n";
+
     mail($to,$email_subject,$email_body,$headers);
+
     header("location: ContactMe.html");
+
  ?>
