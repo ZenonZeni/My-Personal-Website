bc0c3449b5a006c309535743f329f3118f1c8438
Christopher Nguyen <ZenonZeni@hotmail.com>
2021-02-15 19:15:23 -0500

    php changes

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -7,16 +7,19 @@
 
       $email_subject = "New Form Submission";
 
+      $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message: {$country}", "Subject:", $subject];
+      $body = join(PHP_EOL, $bodyParagraphs);
+
       $email_body = 'From: zenonzeni.ca' . "\r\n" .
                     'Reply-To: zenonzeni.ca' . "\r\n" .
-                    'X-Mailer: PHP/' $subject . phpversion();
+                    'X-Mailer: PHP/' . phpversion();
 
     $to = 'zenonzeni@gmail.com';
 
     $headers = "From: zenonzeni.ca \r\n" .
                 "Reply-To: $visitor_email \r\n";
 
-    $send_contact= mail($to,$email_subject,$email_body,$headers);
+    $send_contact= mail($to,$email_subject,$body,$headers);
 
     if($send_contact){
       header('Location: ContactMe.html');
