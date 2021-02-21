db09e2950b8dd6535f9224d9588f955e173f0b32
Christopher Nguyen <ZenonZeni@hotmail.com>
2021-02-21 07:25:08 -0500

    php syntax error fixes

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -4,14 +4,12 @@
       $visitor_email = $_POST['email'];
 
       $email_from = 'opc@zenonzeni.ca';
-      $email_subject = "New Form Submission"
-      $email_body = "User Name:". $name . "\n".
-                    "User Email:". $visitor_email."\n".
-                    "User subject:". $subject. "\n";
+      $email_subject = "New Form Submission";
+      $txt = "User Name:".$name.".\n"."User Email:".$visitor_email."\n"."User subject:". $subject;
 
-    $to = "ZenonZeni@gmail.com"
+    $to = "ZenonZeni@gmail.com";
     $headers = "From: $email_from \r\n";
     $headers .= "Reply-To: $visitor_email \r\n";
-    mail($to,$email_subject,$email_body,$headers);
-    header("location: ContactMe.html")
+    mail($to,$email_subject,$txt,$headers);
+    header("location: ContactMe.html");
  ?>
