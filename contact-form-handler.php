c3d61469fca156ead43d887dc0bd71d6837a1aa4
Christopher Nguyen <ZenonZeni@hotmail.com>
2021-02-21 07:08:12 -0500

    contact form setup for sending email

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,12 +1,17 @@
 <?php
-    $to = "zenonzeni@gmail.com";
-    $subject = "My subject";
-    $txt = "Hello world!";
-    $headers = "From: opc@zenonzeni.ca" . "\r\n" .
-    "CC: opc@zenonzeni.ca";
+      $name = $_POST['name'];
+      $subject = $_POST['message'];
+      $visitor_email = $_POST['email'];
 
-    echo mail($to,$subject,$txt,$headers);
+      $email_from = 'opc@zenonzeni.ca';
+      $email_subject = "New Form Submission"
+      $email_body = "User Name: $name. \n".
+                    "User Email: $visitor_email.\n"
+                    "User subject: $subject. \n";
 
-    echo test;
-
+    $to = "ZenonZeni@gmail.com"
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("location: ContactMe.html")
  ?>
