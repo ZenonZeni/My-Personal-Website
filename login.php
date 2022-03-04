2c88137ac3e4bab72adbd489ab083672ded2104d
Christopher Nguyen <61134161+ZenonZeni@users.noreply.github.com>
2022-03-04 17:59:18 -0500

    Create Login Page
    Still under development

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+			<!-- Global site tag (gtag.js) - Google Analytics -->
+			<script async src="https://www.googletagmanager.com/gtag/js?id=G-B889QJFVCJ"></script>
+			<script>
+			  window.dataLayer = window.dataLayer || [];
+			  function gtag(){dataLayer.push(arguments);}
+			  gtag('js', new Date());
+			gtag('config', 'G-B889QJFVCJ');
+			</script>
+			<link rel="icon" href="My-Personal-Website/Images/favicon.png">
+			<title> Login </title>
+</head>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "<center><p>Login Page Under Development</p></center>";
+?>
+
+</body>
+</html>
