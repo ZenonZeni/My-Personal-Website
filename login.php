79d1babf37b517f801d3e26bd04ba897806245ec
Christopher Nguyen <61134161+ZenonZeni@users.noreply.github.com>
2022-04-02 23:33:44 -0400

    Updated Login Page
    - CSS changes
    - Added another PHP file for login verification.
    - Implement in summer

--- login.php
+++ login.php
@@ -11,18 +11,33 @@
 			gtag('config', 'G-B889QJFVCJ');
 			</script>
 			<link rel="icon" href="My-Personal-Website/Images/favicon.png">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 			<title> Login </title>
 </head>
 <body>
 
-<center> <h1>Login</h1> </center>
+<div id="title" style="padding-top:10%;">
+<center> <h1>Booking System</h1> </center>
+<center> <h2>Under Testing</h2> </center>
+</div>
 
-<?php
-echo "<center><p>Login Page Under Development</p> 
-<input type=\"text\" id=\"username\" placeholder=\"username\">
-<br>
-<input type=\"text\" id=\"password\" placeholder=\"password\"> </center>";
-?>
+<div style="padding-left:25%; padding-right:25%;">
+<form class="form-horizontal" action="LoginSystem/verifyLogin.php" method="POST">
+  <div class="form-group">
+    <label for="email">UserName:</label>
+    <input type="email" class="form-control" id="email" name="email">
+  </div>
+  <div class="form-group" style="padding-top:10px;">
+    <label for="pwd">Password:</label>
+    <input type="password" class="form-control" id="password" name="password">
+  </div>
+  <div class="checkbox" style="padding-top:10px;">
+    <label><input type="checkbox"> Remember me</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
 
 </body>
 </html>
